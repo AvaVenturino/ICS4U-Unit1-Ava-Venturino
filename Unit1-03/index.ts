242cef06aedde35c764af3685956f8b70b9f6187
Ava Venturino <ava.venturino@mths.ca>
2024-02-23 15:03:46 +0000

    added stuff

--- Unit1-03/index.ts
+++ Unit1-03/index.ts
@@ -1,30 +1,57 @@
 /*
  * This is a program that calculates the amount of energy produced when given the mass.
- *
- * @author  Ava Venturino
- * @version 1.0
- * @since   2024-02-22
- */
+ *@author  Ava Venturino 
+ * @version 1.0 
+ * @since   2024-02-22 
+ */ 
+  
+import { createPrompt } from 'bun-promptx' 
 
-import { createPrompt } from 'bun-promptx'
 
-// input
-const subPizzaOrSoup = createPrompt("Are you heating sub, pizza, or soup?: ")
-// error checking
-if (isNaN(subPizzaOrSoup) == false || mass =< 0 || mass >= 0 ) {
-console.log("Invalid input.")
+const subPizzaOrSoup = createPrompt("Are you heating sub, pizza, or soup?: ");
+const numberOfFoodString = createPrompt(`How many ${subPizzaOrSoup}(s) are you cooking? (max:3): `);
+
+// Error checking
+const numberOfFood = parseFloat(numberOfFoodString.value || "-1");
+
+// Process
+let time;
+
+if (subPizzaOrSoup === "sub") {
+	if (numberOfFood === 1)
+		time = 1;
+        else if (numberOfFood === 2)
+		time = 1.5;
+        else if (numberOfFood === 3)
+		time = 2;
+        else
+		console.log("Invalid input");
+} else if (subPizzaOrSoup === "pizza") {
+	if (numberOfFood === 1)
+		time = 0.45;
+	else if (numberOfFood === 2)
+		time = 1.08;
+	else if (numberOfFood === 3)
+		time = 1.30;
+	else
+		console.log("Invalid input");
+} else if (subPizzaOrSoup === "soup") {
+	if (numberOfFood === 1)
+		time = 1.45;
+	else if (numberOfFood === 2)
+		time = 2.18;
+	else if (numberOfFood === 3)
+		time = 3.30;
+	else
+		console.log("Invalid input");
 } else {
-	const numberOfFoodString = createPrompt("How many ${subPizzaOrSoup}(s) are you cooking? (max:3): ")
-	// error checking
-	const numberOfFood  = parseFloat(numberOfFoodString.value || "-1")
-	if (isNaN(numberOfFood) == true || mass < 0 || mass > 3 ) {
-	console.log("Invalid input.")
-	} else {
-		// process
-		const time  =  
-	}
-// output
-console.log(` ${mass} kg of mass would produce ${energy.toExponential(3)} J of energy.`)
+	console.log("Invalid Input");
 }
+// Output
+if (time !== undefined) {
+	console.log(`The total cook time is ${time} minutes.`);
+} else {
+	console.log("Cook time could not be determined.");
+}
+console.log("\nDone.");
 
-console.log("\nDone.")
