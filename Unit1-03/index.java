24cb78f82b95edd397a511f5f19c8e8419ae767d
Ava Venturino <ava.venturino@mths.ca>
2024-02-27 00:08:50 +0000

    fixed java

--- Unit1-03/index.java
+++ Unit1-03/index.java
@@ -12,42 +12,67 @@ import java.util.Scanner;
 /**
  * This is a standard calculation program.
  */
-final class MicrowaveCooking {
-    
-     /**
-     * number of food, 1
+final class Index {
+
+    /**
+     * Number of food, 1.
      */
-     public static final double NUMBEROFFOOD1 = 1
+    public static final int NUMBEROFFOOD1 = 1;
 
-     /**   
-      * number of food, 2
-      */
-      public static final double NUMBEROFFOOD2 = 2
+    /**
+     * Number of food, 2.
+     */
+    public static final int NUMBEROFFOOD2 = 2;
 
-     /**   
-      * number of food, 3
-      */
-      public static final double NUMBEROFFOOD3 = 3
+    /**
+     * Number of food, 3.
+     */
+    public static final int NUMBEROFFOOD3 = 3;
 
-     /**   
-      * time for food, 1
-      */
-      public static final double TIME1 = 1
+    /**
+     * Time for food, 1.
+     */
+    public static final int TIME1 = 1;
 
-     /**   
-      * time for food, 0         
-      */              
-      public static final double TIME0 = 0
+    /**
+     * Time for food, 0.
+     */
+    public static final int TIME0 = 0;
 
-     /**         
-      * time for food, 2                       
-      */                                  
-      public static final double TIME2 = 2
+    /**
+     * Time for food, 2.
+     */
+    public static final int TIME2 = 2;
 
-     /**         
-      * time for food, 3                  
-      */                                  
-      public static final double TIME3 = 3
+    /**
+     * Time for food, 3.
+     */
+    public static final int TIME3 = 3;
+
+    /**
+     * Time for food, 8.
+     */
+    public static final int TIME8 = 8;
+
+    /**
+     * Time for food, 18.
+     */
+    public static final int TIME18 = 18;
+
+    /**
+     * Time for food, 20.
+     */
+    public static final int TIME20 = 20;
+
+    /**
+     * Time for food, 30.
+     */
+    public static final int TIME30 = 30;
+
+    /**
+     * Time for food, 45.
+     */
+    public static final int TIME45 = 45;
 
     /**
     * Prevent instantiation.
@@ -58,9 +83,9 @@ final class MicrowaveCooking {
     *
     */
 
-    private MicrowaveCooking() {
+    private Index() {
         throw new IllegalStateException("Cannot be instantiated");
-	}
+    }
     /**
     * The starting main() function.
     *
@@ -69,72 +94,68 @@ final class MicrowaveCooking {
 
     public static void main(final String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        final Scanner scanner = new Scanner(System.in);
 
         // Prompt user for the type of food (sub, pizza, or soup)
         System.out.print("Are you heating sub, pizza, or soup?: ");
-        String subPizzaOrSoup = scanner.nextLine();
+        final String subPizzaOrSoup = scanner.nextLine();
 
         // Prompt user for the number of items to cook
         System.out.print("How many " + subPizzaOrSoup
-			+ "(s) are you cooking? (max: 3): ");
-        int numberOfFood = Integer.parseInt(scanner.nextLine());
+                + "(s) are you cooking? (max: 3): ");
+        final int numberOfFood = Integer.parseInt(scanner.nextLine());
 
         // Process
         int minute = 0;
         int second = 0;
 
-        if (subPizzaOrSoup.equals("sub")) {
-		    if (numberOfFood == NUMBEROFFOOD1) {
-			    minute = TIME1;
-			    second = TIME0;
-		    } else if (numberOfFood == NUMBEROFFOOD2) {
-			    minute = TIME1;
-			    second = 30;
-		    } else if (numberOfFood == NUMBEROFFOOD3) {
-			    minute = TIME2;
-			    second = TIME0;
-		    } else {
-			    System.out.println("Invalid input");
-			    return;
-		    }
-	    } else if (subPizzaOrSoup.equals("pizza")) {
-		    if (numberOfFood == NUMBEROFFOOD1) {
-			    minute = TIME0;
-			    second = 45;
-		    } else if (numberOfFood == NUMBEROFFOOD2) {
-			    minute = TIME1;
-			    second = 8;
-		    } else if (numberOfFood == NUMBEROFFOOD3) {
-			    minute = TIME1;
-			    second = 20;
-		    } else {
-			    System.out.println("Invalid input");
-			    return;
-		    }
-	    } else if (subPizzaOrSoup.equals("soup")) {
-		    if (numberOfFood == NUMBEROFFOOD1) {
-			    minute = TIME1;
-			    second = 45;
-		    } else if (numberOfFood == NUMBEROFFOOD2) {
-			    minute = TIME2;
-			    second = 18;
-		    } else if (numberOfFood == NUMBEROFFOOD3) {
-			    minute = TIME3;
-			    second = 20;
-		    } else {
-			    System.out.println("Invalid input");
-			    return;
-		    }
-	    } else {
-		    System.out.println("Invalid Input");
-		    return;
-	    }
+        if ("sub".equals(subPizzaOrSoup)) {
+            if (numberOfFood == NUMBEROFFOOD1) {
+                minute = TIME1;
+                second = TIME0;
+            } else if (numberOfFood == NUMBEROFFOOD2) {
+                minute = TIME1;
+                second = TIME30;
+            } else if (numberOfFood == NUMBEROFFOOD3) {
+                minute = TIME2;
+                second = TIME0;
+            } else {
+                System.out.println("Invalid input");
+            }
+        } else if ("pizza".equals(subPizzaOrSoup)) {
+            if (numberOfFood == NUMBEROFFOOD1) {
+                minute = TIME0;
+                second = TIME45;
+            } else if (numberOfFood == NUMBEROFFOOD2) {
+                minute = TIME1;
+                second = TIME8;
+            } else if (numberOfFood == NUMBEROFFOOD3) {
+                minute = TIME1;
+                second = TIME20;
+            } else {
+                System.out.println("Not a valid answer");
+            }
+        } else if ("soup".equals(subPizzaOrSoup)) {
+            if (numberOfFood == NUMBEROFFOOD1) {
+                minute = TIME1;
+                second = TIME45;
+            } else if (numberOfFood == NUMBEROFFOOD2) {
+                minute = TIME2;
+                second = TIME18;
+            } else if (numberOfFood == NUMBEROFFOOD3) {
+                minute = TIME3;
+                second = TIME20;
+            } else {
+                System.out.println("Invalid answer");
+            }
+        } else {
+            System.out.println("Input is invalid, try again");
+        }
 
-	    // Output
+        // Output
 
-	    System.out.println("The total cook time is " + minute 
-			    + " minutes and " + second + " seconds.\n");
-	    System.out.println("Done.");
+        System.out.println("The total cook time is " + minute
+                + " minutes and " + second + " seconds.\n");
+        System.out.println("Done.");
     }
 }
